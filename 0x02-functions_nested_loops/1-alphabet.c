fd5c961c05847c82e8ed47d872b7f45e08af6337
Esther Daniel <tunrayodaniels@gmail.com>
2023-06-21 18:08:21 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - printing alphabeths
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+}
