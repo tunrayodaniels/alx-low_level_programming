77937f87b973f1cfc144cf7bf189cf22ec628e13
Esther Daniel <tunrayodaniels@gmail.com>
2023-06-22 17:24:24 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - low case checker
+ * @c: c is the variable being checked
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'a' && c <= 'z');
+
+}
