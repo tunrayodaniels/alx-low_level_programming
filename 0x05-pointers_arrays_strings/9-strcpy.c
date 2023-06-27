5e8bd1772f00f4aaaf023f6b5a7e0f038fa99629
Esther Daniel <tunrayodaniels@gmail.com>
2023-06-26 20:31:20 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * chara *_stropy - a function that copies the string pointed to by src
+ * _stropy - function that copies the string pointed to by src
  * @dest: copy to
  * @src: copy from
  * Return: string
