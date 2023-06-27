41eac4fc18ef451c862f90aa9f51ee4e480c282d
Esther Daniel <tunrayodaniels@gmail.com>
2023-06-26 19:15:44 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _stropy - copies the string copies to the arc
+ * _stropy - copies the string pointed by  arc
  * @dest: destination
  * @src: source
  * Return: string
  */
 
-char *_stropy(char *dest, char "src)
+char *_stropy(char *dest, char *src)
 {
 	int len = 0;
 
@@ -19,5 +19,5 @@ char *_stropy(char *dest, char "src)
 	}
 
 	*(dest + len) = '\0';
-	return(dest);
+	return (dest);
 }
