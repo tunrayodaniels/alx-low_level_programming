2886f0e9a39d0382556e50c5f41434bd749b676d
Esther Daniel <tunrayodaniels@gmail.com>
2023-06-27 08:16:39 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,44 +1,32 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _atoi - convert a string to an integer
- * @s: string to be converted
+ * main - check the code
  *
- * Return: the int converted from the string
+ * Return: Always 0;
  */
-int _atoi(char *s)
+int main(void)
 {
-	int i, d, n, len, f, digit;
+	int nb;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-	while (i < len && f == 0)
-	{
-		if (s[i] == '_')
-			++d;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n 	* 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-
-	if (f == 0)
-		return (0);
-	
-	return (n);
+	nb = _atoi("98");
+	printf("%d\n", nb);
+	nb = _atoi("-402");
+	printf("%d\n", nb);
+	nb = _atoi("              ------++++++-----+++++--98");
+	printf("%d\n", nb);
+	nb = _atoi("214748364");
+	printf("%d\n", nb);
+	nb = _atoi("0");
+	printf("%d\n", nb);
+	nb = _atoi("Suite 402");
+	printf("%d\n", nb);
+	nb = _atoi("        +    +  -   -98 Battery Street; San Francisco, CA 941
+	11 - USA         ");
+	printf("%d\n",  nb);
+	nb = _atoi("---++++ -+ Sui - te -  402 #cisfun :)");
+	printf("%d\n", nb);
+	return (0);
 }
+
