20874dae41df9311adb533e3098bf619c505b75b
Esther Daniel <tunrayodaniels@gmail.com>
2023-06-29 18:38:48 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,24 +3,23 @@
 /**
  * print_number - prints an integer;
  * @n: integer to be printed;
- * 
- * Return: (void)
  */
+
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int x;
 
 	if (n < 0)
 	{
-		n1 = -n;
+		x = -n;
 		_putchar('-');
 	} else
 	{
-		n1 = n;
+		x = n;
 	}
 
-	if (n1 / 10)
-		print_number(n1 / 10);
+	if (x / 10)
+		print_number(x / 10);
 
-	_putchar((n1 % 10) + '0');
+	_putchar((x % 10) + '0');
 }
