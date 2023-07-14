c821ebe85bf41f6272251c0b5e2ecac76f1deabb
Esther Daniel <tunrayodaniels@gmail.com>
2023-07-13 17:12:42 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * *string_nconcat - concatenates n bytes of a string to another string
+ * *string_nconcat - a function that concatenates n bytes of a string to another string
  * @s1: string to append to
  * @s2: string to concatenate from
  * @n: number of bytes from s2 to concatenate to s1
@@ -11,19 +11,15 @@
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
-
 {
-
 	char *s;
 
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
-
 		len1++;
 
 	while (s2 && s2[len2])
-
 		len2++;
 
 	if (n < len2)
@@ -39,15 +35,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 
 	while (i < len1)
-
 	{
-
 		s[i] = s1[i];
 
 		i++;
-
 	}
-
 	while (n < len2 && i < (len1 + n))
 
 		s[i++] = s2[j++];
