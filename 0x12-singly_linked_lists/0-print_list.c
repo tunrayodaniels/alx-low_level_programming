e2d3f55e24d81e08aaf10424f45e88f3ea6b1fda
Daniel Esther <tunrayodaniels@gmail.com>
2023-07-27 17:28:02 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t x = 0;
+	size_t s = 0;
 	while (h)
 	{
 		if (!h->str)
@@ -18,7 +18,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		x++;
+		s++;
 	}
-	return (x);
+	return (s);
 }
