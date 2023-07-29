dcc3efb0a436131eefa180cf1d11bbd410ef7b0b
Daniel Esther <tunrayodaniels@gmail.com>
2023-07-29 01:49:55 -0700

    statics library

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,4 @@
 #ifndef MAIN_H
-
 #define MAIN_H
 
 int _putchar(char c);
@@ -33,6 +32,7 @@ int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 
 char *_memcpy(char *dest, char *src, unsigned int n);
+
 char *_strchr(char *s, char c);
 
 unsigned int _strspn(char *s, char *accept);
